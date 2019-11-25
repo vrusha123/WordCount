443cc124793a850f2680e64c742ddd176f60741f
vrusha123 <30957901+vrusha123@users.noreply.github.com>
2019-11-25 11:36:43 +0530

    Update WordCount.exs

--- WordCount.exs
+++ WordCount.exs
@@ -1,9 +1,4 @@
 defmodule Words do
-  @doc """
-  Count the number of words in the sentence.
-
-  Words are compared case-insensitively.
-  """
   @spec count(String.t) :: map()
   def count(sentence) do
     tokens = sentence 
